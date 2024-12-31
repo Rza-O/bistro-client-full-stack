9a7068f2ce94ff0671f6ac4cfc0947a85774a661
appoclayptor <shahreza1414@gmail.com>
2024-12-31 12:16:43 +0600

    fetched menu  from server

--- src/Components/FoodCard/FoodCard.jsx
+++ src/Components/FoodCard/FoodCard.jsx
@@ -10,11 +10,11 @@ const FoodCard = ({ item }) => {
                alt="Shoes" />
          </figure>
          <p className='bg-slate-900 text-white absolute right-3 mr-4 mt-4 p-1'>${price}</p>
-         <div className="card-body text-center">
+         <div className="card-body">
             <h2 className="card-title">{name}</h2>
             <p>{recipe}</p>
             <div className="card-actions justify-end">
-               <button className="btn btn-primary">Add to cart</button>
+               <button className="btn btn-outline border-0 border-b-4 border-orange-400 mt-4">Add to cart</button>
             </div>
          </div>
       </div>
--- src/Pages/Order/Order/Order.jsx
+++ src/Pages/Order/Order/Order.jsx
@@ -6,10 +6,11 @@ import 'react-tabs/style/react-tabs.css';
 import { useMenu } from '../../../Hooks/useMenu';
 import OrderTab from '../OrderTab/OrderTab';
 import { useParams } from 'react-router-dom';
+import { Helmet } from 'react-helmet-async';
 
 
 const Order = () => {
-   const categories = ['salad', 'pizza', 'soup', 'dessert', 'drinks'];
+   const categories = ['salads', 'pizzas', 'soups', 'desserts', 'drinks'];
    const { category } = useParams();
    const initialIndex = categories.indexOf(category);
    const [tabIndex, setTabIndex] = useState(initialIndex);
@@ -21,6 +22,9 @@ const Order = () => {
    const drinks = menu.filter(item => item.category === 'drinks');
    return (
       <div>
+         <Helmet>
+            <title>Bistro | Order Food</title>
+         </Helmet>
          <Cover img={orderCover} title={'Order Food'}></Cover>
          <Tabs className={'flex flex-col justify-center items-center my-8'} defaultIndex={tabIndex} onSelect={(index) => setTabIndex(index)}>
             <TabList>
