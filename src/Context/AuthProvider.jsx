95b881813f2219bacc6c580e53859df17e11f7bf
appoclayptor <shahreza1414@gmail.com>
2025-01-09 03:24:00 +0600

    implement secure axios instance with request/response interceptors for token management

--- src/Context/AuthProvider.jsx
+++ src/Context/AuthProvider.jsx
@@ -1,6 +1,7 @@
 import React, { createContext, useEffect, useState } from 'react';
 import { createUserWithEmailAndPassword, getAuth, GoogleAuthProvider, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut, updateProfile } from "firebase/auth";
 import { app } from '../Firebase/firebase.config';
+import useAxiosPublic from '../Hooks/useAxiosPublic';
 
 export const AuthContext = createContext(null);
 const auth = getAuth(app);
@@ -9,6 +10,7 @@ const AuthProvider = ({ children }) => {
    const [user, setUser] = useState(null);
    const [loading, setLoading] = useState(true);
    const googleProvider = new GoogleAuthProvider();
+   const axiosPublic = useAxiosPublic();
 
    const googleSignIn = () => { 
       setLoading(true);
@@ -39,14 +41,26 @@ const AuthProvider = ({ children }) => {
    useEffect(() => {
       const unsubscribe = onAuthStateChanged(auth, currentUser => {
          setUser(currentUser);
-         console.log('currentUser=>', currentUser);
+         if (currentUser) {
+            // get token and store in client side
+            const userInfo = currentUser.email;
+            axiosPublic.post('jwt', userInfo)
+               .then(res => {
+                  if (res.data.token) {
+                     localStorage.setItem('access-token', res.data.token);
+               }
+            })
+         } else {
+            // TODO: remove token(if token stored in the client side)
+            localStorage.removeItem('access-token')
+         }
          setLoading(false);
       })
    
       return () => {
          unsubscribe();
       }
-   }, []);
+   }, [axiosPublic]);
 
    const authInfo = {
       user,
